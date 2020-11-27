b6d493e9fb41a12bb073ed5bd47d8c82098cb05f
Gabriel-Miranda-98 <72518066+Gabriel-Miranda-98@users.noreply.github.com>
2020-11-27 20:55:34 -0300

    Gabriel Commit

--- back_end/conexap.php
+++ back_end/conexap.php
@@ -1,11 +1,16 @@
 <?php
+include('Tela_login.php');
+if (empty ($_POST['usuario'])|| empty ($_POST['senha'])) {
+    header('Location: ../Tela_login.php');
+    exit();
+}
 
 define("HOST","127.0.0.1");
 define("USUARIO","Gabriel");
 define("SENHA","Gb@30173572");
 define("DB","Loja");
 
-$conexo=mysqli_connect("HOST","USUARIO","SENHA","DB") or die("Não Foi Possivel conectar ao banco");
+$conexo= mysqli_connect(HOST,USUARIO,SENHA,DB) or die("Não Foi Possivel conectar ao banco");
 
-
+ 
 ?>
