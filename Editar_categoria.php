590fd575b7f835e181e041a85c4e1e5cad22d991
LeticiaRosa <37852713+LeticiaRosa@users.noreply.github.com>
2020-12-14 23:27:39 -0300

    DataTable Concluido

--- Editar_categoria.php
+++ Editar_categoria.php
@@ -1,67 +1,68 @@
 <!DOCTYPE html>
 
-    <head>
-    <meta charset="utf-8"/>
+<head>
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200&display=swap" rel="stylesheet">      
+    <link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css" />
+
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
     <link rel="stylesheet" type="text/css" href="css/css_editar_categoria.css">
-   
- 
+
+</head>
+<html>
+
+<body>
 
     <title>Editar Categoria</title>
     </head>
+
     <body>
 
-<?php 
-   include_once("menu.php");
-?>          
+        <?php
+        include_once("menu.php");
+        ?>
         <section class="cover-form">
-        <div class="form-container">
-        <h1>Categorias</h1>
-       
-        <div class="col">
-        <p>Pesquisar Produto</p>
-<input type="text" name="pesquisa" id="pesq" onkeyup='searchTable()'/>
-        
-</div>
-        <div class="form-wraper" id="visualizarDados">
-            
-        <table id ="products-table">
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th >Descricao</th>
-                <th >Status</th>
-                <th class="sumir">Observação</th>
-                <th class="sumir"> Usuario</th>
-                <th class="sumir">Data Cadastro</th>
-                
-            </tr>
-        </thead>
-<tbody>
+            <div class="form-container">
+                <h1>Editar Categoria</h1>
+
+                <div class="form-wraper-1">
+
+                    <table id="products-table">
+                        <thead class="cabeça">
+                            <tr>
+                                <th>Nome</th>
+                                <th>Descricao</th>
+                                <th>Status</th>
+                                <th class="sumir">Observação</th>
+                                <th class="sumir"> Usuario</th>
+                                <th class="sumir">Data Cadastro</th>
+
+                            </tr>
+                        </thead>
+                        <tbody id="visualizarDados">
 
 
-</tbody>
-</table>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!--container bg-->
+        </section>
+        <!--cover-form-->
 
+
+        <div id="openModal" class="modalDialog">
+            <div><a href="#close" title="Close" class="close">X</a>
+                <?php include_once("Tela_altera_categoria.php");  ?>
+            </div>
         </div>
-        
-        
-        </div><!--container bg-->
-    </section><!--cover-form-->
 
- <div id="openModal" class="modalDialog">
-    <div><a href="#close" title="Close" class="close">X</a>    
-    <?php  include_once("Tela_altera_categoria.php");  ?>
-    </div>
-</div>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/lista_categorias.js"></script>
+        <script type="text/javascript" src="DataTables/datatables.min.js"></script>
 
+    </body>
 
-<script type="text/javascript" src="js/filtar_tabela.js"></script>
-    <script type="text/javascript" src="js/jquery.js"></script>
-     <script type="text/javascript" src="js/lista_categorias.js"></script>
-	<script type="text/javascript" src="js/jquery-ui.min.js"></script>    
-    
-
-</body>
-    </html>
+</html>
--- js/lista_categorias.js
+++ js/lista_categorias.js
@@ -1,107 +1,158 @@
-$(window).on("load",$(async function() {
+$(window).on("load", $(async function() {
     // Atribui evento e função para limpeza dos campos
- // $('#clicar').on('input', limpaCampos);
-    
- var nomes = [];
- await $.ajax({
-     url: "back_end/busca_autocomplete.php",
-     dataType: "json",
-     data: {
-         acao: 'categoria'
-     },
-     success: function(data) {
-       
-        nome= data.map(d=>d.NOME); 
-       descricao= data.map(d=>d.DESCRICAO); 
-       STATUS= data.map(d=>d.STATUS);          
-       observacao= data.map(d=>d.OBSERVACAO); 
-       USUARIO= data.map(d=>d.USUARIO);  
-       DATA_CADASTRO= data.map(d=>d.DATA_CADASTRO);   
-      for(i=0;i<data.length;i++){
-        var newRow = $("<tr>");
-        var cols = "";
-  
-      cols += '<td>'+nome[i]+'</td>';
-      cols += '<td>'+descricao[i]+'</td>';
-      cols += '<td>'+STATUS[i]+'</td>';
-      cols += '<td class="sumir">'+observacao[i]+'</td>';
-      cols += '<td class="sumir">'+USUARIO[i]+'</td>';
-      cols += '<td class="sumir">'+DATA_CADASTRO[i]+'</td>';
-      
-  
-      newRow.append(cols);
-      $("#products-table").append(newRow);
+    // $('#clicar').on('input', limpaCampos);
 
+    var nomes = [];
+    await $.ajax({
+        url: "back_end/busca_autocomplete.php",
+        dataType: "json",
+        data: {
+            acao: 'categoria'
+        },
+        success: function(data) {
+
+            nome = data.map(d => d.NOME);
+            descricao = data.map(d => d.DESCRICAO);
+            STATUS = data.map(d => d.STATUS);
+            observacao = data.map(d => d.OBSERVACAO);
+            USUARIO = data.map(d => d.USUARIO);
+            DATA_CADASTRO = data.map(d => d.DATA_CADASTRO);
+            for (i = 0; i < data.length; i++) {
+                var newRow = $('<tr class = "corpo" >');
+                var cols = "";
+
+                cols += '<td>' + nome[i] + '</td>';
+                cols += '<td>' + descricao[i] + '</td>';
+                cols += '<td>' + STATUS[i] + '</td>';
+                cols += '<td class="sumir">' + observacao[i] + '</td>';
+                cols += '<td class="sumir">' + USUARIO[i] + '</td>';
+                cols += '<td class="sumir">' + DATA_CADASTRO[i] + '</td>';
+
+
+                newRow.append(cols);
+                $("#products-table").append(newRow);
+            }
+            $(document).ready(function() {
+
+                $('#products-table').dataTable({
+                    "bJQueryUI": true,
+                    "sPaginationType": "full_numbers",
+                    "sDom": '<"H"Tlfr>t<"F"ip>',
+                    "oTableTools": {
+                        "sSwfPath": "../../js/DataTables-1.9.4/extras/TableTools/media/swf/copy_csv_xls_pdf.swf",
+                        "aButtons": [{
+                                "sExtends": "xls",
+                                "sButtonText": "Exportar para Excel",
+                                "sTitle": "Usuarios",
+                                "mColumns": [0, 1, 2, 3]
+                            },
+                            {
+                                "sExtends": "pdf",
+                                "sButtonText": "Exportar para PDF",
+                                "sTitle": "Usuarios",
+                                "sPdfOrientation": "landscape",
+                                "mColumns": [0, 1, 2, 3]
+                            }
+                        ]
+                    },
+                    "oLanguage": {
+                        "sLengthMenu": "Mostrar _MENU_ registros por página",
+                        "sZeroRecords": "Nenhum registro encontrado",
+                        "sInfo": "Mostrando _START_ / _END_ de _TOTAL_ registro(s)",
+                        "sInfoEmpty": "Mostrando 0 / 0 de 0 registros",
+                        "sInfoFiltered": "(filtrado de _MAX_ registros)",
+                        "sSearch": "Pesquisar: ",
+                        "oPaginate": {
+                            "sFirst": "Início",
+                            "sPrevious": "Anterior",
+                            "sNext": "Próximo",
+                            "sLast": "Último"
+                        }
+                    },
+                    "aaSorting": [
+                        [0, 'desc']
+                    ],
+                    "aoColumnDefs": [
+                        { "sType": "num-html", "aTargets": [0] }
+
+                    ]
+                });
+            }); //fim jquery
+
+
+
+
+
+
+
+
+
+        }
+    });
+
+}));
+
+
+$(window).on("click", (function() {
+
+    var tabela = document.getElementById("products-table");
+    var linhas = tabela.getElementsByTagName("tr");
+    for (var i = 0; i < linhas.length; i++) {
+        var linha = linhas[i];
+
+        linha.addEventListener("click", function() {
+            //Adicionar ao atual
+
+            selLinha(this, false); //Selecione apenas um
+            //selLinha(this, true); //Selecione quantos quiser
+        });
     }
-    $(document).ready(function() {
-      $('#products-table').dataTable();
-} );
-     }
- });
 
- } ));
+    /**
+    Caso passe true, você pode selecionar multiplas linhas.
+    Caso passe false, você só pode selecionar uma linha por vez.
+    **/
+    function selLinha(linha, multiplos) {
 
-
- $(window).on("click",(function() {
-
-var tabela = document.getElementById("products-table");
-var linhas = tabela.getElementsByTagName("tr");
-for(var i = 0; i < linhas.length; i++){
-    var linha = linhas[i];
-   
-  linha.addEventListener("click", function(){
-      //Adicionar ao atual
-     
-		selLinha(this, false); //Selecione apenas um
-    //selLinha(this, true); //Selecione quantos quiser
-	});
-}
-
-/**
-Caso passe true, você pode selecionar multiplas linhas.
-Caso passe false, você só pode selecionar uma linha por vez.
-**/
-function selLinha(linha, multiplos){
-    
-    if(!multiplos){
-  	var linhas = linha.parentElement.getElementsByTagName("tr");
-    for(var i = 0; i < linhas.length; i++){
-      var linha_ = linhas[i];
-      linha_.classList.remove("selecionado");    
+        if (!multiplos) {
+            var linhas = linha.parentElement.getElementsByTagName("tr");
+            for (var i = 0; i < linhas.length; i++) {
+                var linha_ = linhas[i];
+                linha_.classList.remove("selecionado");
+            }
+        }
+        linha.classList.toggle("selecionado");
     }
-  }
-  linha.classList.toggle("selecionado");
-}
 
-/**
-Exemplo de como capturar os dados
-**/
-var btnVisualizar = document.getElementById("visualizarDados");
+    /**
+    Exemplo de como capturar os dados
+    **/
+    var btnVisualizar = document.getElementById("visualizarDados");
 
-var nome = "";
-btnVisualizar.addEventListener("click", function(){
-    var selecionados = tabela.getElementsByClassName("selecionado");
-    
-  //Verificar se está selecionado
-  
-  for(var i = 0; i < selecionados.length; i++){
-  	var selecionado = selecionados[i];
-    selecionado = selecionado.getElementsByTagName("td");
-    
-  }
-  if (selecionado[1].innerHTML  !== null ){
-  window.location.replace("#openModal");
-        
-  document.getElementById('nome').value = selecionado[1].innerHTML;
-  document.getElementById('descricao').value = selecionado[2].innerHTML;
- document.getElementById('observacao').value = selecionado[3].innerHTML;
-  
-  
-}
-});
+    var nome = "";
+    btnVisualizar.addEventListener("click", function() {
+        var selecionados = tabela.getElementsByClassName("selecionado");
 
-function fechamodal(){
-  $('#modal').css("display", "none");
-}
+        //Verificar se está selecionado
 
- }));
+        for (var i = 0; i < selecionados.length; i++) {
+            var selecionado = selecionados[i];
+            selecionado = selecionado.getElementsByTagName("td");
+
+        }
+        if (selecionado[1].innerHTML !== null) {
+            window.location.replace("#openModal");
+
+            document.getElementById('nome').value = selecionado[1].innerHTML;
+            document.getElementById('descricao').value = selecionado[2].innerHTML;
+            document.getElementById('observacao').value = selecionado[3].innerHTML;
+
+
+        }
+    });
+
+    function fechamodal() {
+        $('#modal').css("display", "none");
+    }
+
+}));
