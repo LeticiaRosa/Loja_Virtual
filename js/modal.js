c4270aae7c88a2ab36a34bcea22adcf3f74f538c
Gabriel-Miranda-98 <72518066+Gabriel-Miranda-98@users.noreply.github.com>
2020-12-16 19:46:46 -0300

    Finalização

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,11 @@
 var select = document.getElementById('pega').value;
-if (select=="Produto inserido com sucesso"){
- // console.log(select);
-  $('#conteiner').css("display", "flex");
+if (select == "Produto inserido com sucesso") {
+    // console.log(select);
+    $('#conteiner').css("display", "flex");
 
 }
 
 
-function fechamodal(){
-  $('#conteiner').css("display", "none");
+function fechamodal() {
+    $('#conteiner').css("display", "none");
 }
