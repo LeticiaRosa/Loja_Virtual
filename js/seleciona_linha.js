802f41de8c3efcda806e7279ca114bc25bd471da
LeticiaRosa <37852713+LeticiaRosa@users.noreply.github.com>
2020-12-17 21:54:41 -0300

    Alteração Letícia

--- js/seleciona_linha.js
+++ js/seleciona_linha.js
@@ -121,39 +121,44 @@ $(window).on("click", (function() {
     **/
     var btnVisualizar = document.getElementById("visualizarDados");
     btnVisualizar.addEventListener("click", function() {
+
         var selecionados = tabela.getElementsByClassName("selecionado");
         //Verificar se está selecionado
 
         for (var i = 0; i < selecionados.length; i++) {
             var selecionado = selecionados[i];
             selecionado = selecionado.getElementsByTagName("td");
+            console.log(selecionado);
         }
 
-        if (selecionado[1].innerHTML !== null) {
 
-            $("#openModal").load("Tela_alterar_produto.php");
-            window.location.replace("#openModal");
-            document.getElementById('id').value = selecionado[0].innerHTML;
-            document.getElementById('nome').value = selecionado[1].innerHTML;
-            document.getElementById('descricao').value = selecionado[2].innerHTML;
-            document.getElementById('id_categoria').value = selecionado[3].innerHTML;
-            document.getElementById('id_sub_categoria').value = selecionado[4].innerHTML;
-            document.getElementById('preco_venda').value = selecionado[5].innerHTML;
-            document.getElementById('preco_custo').value = selecionado[6].innerHTML;
-            document.getElementById('quantidade').value = selecionado[7].innerHTML;
-            document.getElementById('id_fornecedor').value = selecionado[8].innerHTML;
-            document.getElementById('marca').value = selecionado[9].innerHTML;
-            document.getElementById('unidade_medida').value = selecionado[10].innerHTML;
-            document.getElementById('valor_medida').value = selecionado[11].innerHTML;
-            document.getElementById('observacao').value = selecionado[12].innerHTML;
-            document.getElementById('gerar_codigo').value = selecionado[13].innerHTML;
 
-            if (selecionado[0].innerHTML !== selecionado[13].innerHTML) {
-                document.getElementById('radio-2').checked = true;
-            } else {
-                document.getElementById('radio-1').checked = true;
-            }
+        $("#openModal").load("Tela_alterar_produto.php");
+        window.location.replace("#openModal").load("Tela_alterar_produto.php");
+        console.log(selecionado[0].outerText);
+        console.log(selecionado[1].innerHTML);
+        document.getElementById('id').value = 'teste';
+        document.getElementById('id').value = selecionado[0].innerHTML;
+        document.getElementById('nome').value = selecionado[1].innerHTML;
+        document.getElementById('descricao').value = selecionado[2].innerHTML;
+        document.getElementById('id_categoria').value = selecionado[3].innerHTML;
+        document.getElementById('id_sub_categoria').value = selecionado[4].innerHTML;
+        document.getElementById('preco_venda').value = selecionado[5].innerHTML;
+        document.getElementById('preco_custo').value = selecionado[6].innerHTML;
+        document.getElementById('quantidade').value = selecionado[7].innerHTML;
+        document.getElementById('id_fornecedor').value = selecionado[8].innerHTML;
+        document.getElementById('marca').value = selecionado[9].innerHTML;
+        document.getElementById('unidade_medida').value = selecionado[10].innerHTML;
+        document.getElementById('valor_medida').value = selecionado[11].innerHTML;
+        document.getElementById('observacao').value = selecionado[12].innerHTML;
+        document.getElementById('gerar_codigo').value = selecionado[13].innerHTML;
+
+        if (selecionado[0].innerHTML !== selecionado[13].innerHTML) {
+            document.getElementById('radio-2').checked = true;
+        } else {
+            document.getElementById('radio-1').checked = true;
         }
+
     });
 
 
