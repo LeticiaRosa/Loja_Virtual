5e0fe81c64247fd175655a59d2a4bb914a71489b
Gabriel-Miranda-98 <72518066+Gabriel-Miranda-98@users.noreply.github.com>
2021-01-10 22:40:29 -0300

    Botão com imagem

--- Tela_alterar_cliente.php
+++ Tela_alterar_cliente.php
@@ -11,81 +11,89 @@
 
 <body>
 
-<section class="cover-form">
-    <div><a href="Tela_listar_clientes.php" title="Close" class="close">X</a>
-        <div class="form-container">
-            <h1>Editar Cliente</h1>
-            <form method="POST" name="form" action="back_end/cliente.php">
-            <div class="form-wraper">
-                 <div class="col" style="  width: 25%; ">
-                <p>ID_Produto</p>
-                <input type="text" name="id" id="id" value="<?php $id = $_GET['id'];
-                                                            echo $id; ?>" placeholder="id" autocomplete="off" readonly="readonly">
-                </div>
-                <div class="col">
-                    <p>Nome*</p>
-                    <input type="text" name="nome" id="nome" required placeholder="Nome" autocomplete="off">
-                </div>
-                <div class="col">
-                    <p>CPF*</p>
-                    <input type="text" name="cpf" id="cpf" required placeholder="cpf" autocomplete="off">
-                </div>
-                <div class="col">
-                    <p>E-MAIL:</p>
-                    <input type="email" name="E-MAIL" id="E-MAIL" placeholder="E-mail" autocomplete="off">
-                </div>
-                </div>
-                <div class="form-wraper">
-                <div class="col">
-                    <p>Telefone Fixo:</p>
-                    <input type="text" name="fixo" id="fixo" placeholder="Telefone" autocomplete="off">
-                </div>
-                <div class="col">
-                    <p>Telefone celular:</p>
-                    <input type="text" name="celular" id="celular" class="celular" placeholder="Telefone" autocomplete="off">
-                </div>
-                </div>
-                <div class="form-wraper">
-                <div class="col">
-                    <p>Endereço:</p>
-                    <input type="text" name="endereco" id="endereco" placeholder="Endereço" autocomplete="off">
-                </div>
-                <div class="col">
-                    <p>CEP:</p>
-                    <input type="text" name="CEP" id="CEP" placeholder="CEP" autocomplete="off">
-                </div>
-                </div>
-                <div class="form-wraper">
-                <div class="col">
-                    <p>Observação:</p>
-                    <input type="text" name="observacao" id="observacao" placeholder="Observação" autocomplete="off">
-                </div>
-                </div>
+    <section class="cover-form">
+        <div><a href="Tela_listar_clientes.php" title="Close" class="close">X</a>
+            <div class="form-container">
+                <h1>Editar Cliente</h1>
+                <form method="POST" name="form" action="back_end/cliente.php">
+                    <div class="form-wraper">
+                        <div class="col" style="  width: 25%; ">
+                            <p>ID_Produto</p>
+                            <input type="text" name="id" id="id" value="<?php $id = $_GET['id'];
+                                                                        echo $id; ?>" placeholder="id" autocomplete="off" readonly="readonly">
+                        </div>
+                        <div class="col">
+                            <p>Nome*</p>
+                            <input type="text" name="nome" id="nome" required placeholder="Nome" autocomplete="off">
+                        </div>
+                        <div class="col">
+                            <p>CPF*</p>
+                            <input type="text" name="cpf" id="cpf" required placeholder="cpf" autocomplete="off">
+                        </div>
+                        <div class="col">
+                            <p>E-MAIL:</p>
+                            <input type="email" name="E-MAIL" id="E-MAIL" placeholder="E-mail" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="form-wraper">
+                        <div class="col">
+                            <p>Telefone Fixo:</p>
+                            <input type="text" name="fixo" id="fixo" placeholder="Telefone" autocomplete="off">
+                        </div>
+                        <div class="col">
+                            <p>Telefone celular:</p>
+                            <input type="text" name="celular" id="celular" class="celular" placeholder="Telefone" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="form-wraper">
+                        <div class="col">
+                            <p>Endereço:</p>
+                            <input type="text" name="endereco" id="endereco" placeholder="Endereço" autocomplete="off">
+                        </div>
+                        <div class="col">
+                            <p>CEP:</p>
+                            <input type="text" name="CEP" id="CEP" placeholder="CEP" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="form-wraper">
+                        <div class="col">
+                            <p>Observação:</p>
+                            <input type="text" name="observacao" id="observacao" placeholder="Observação" autocomplete="off">
+                        </div>
+                    </div>
 
-                <div class="enviar">
-                 
-                <input type="submit" name="Excluir" id="Excluir" value="Excluir" />
-                <div>
-                <img class = "bntSalvar" src="imagens/icons8_ok_26px.png" ></img>
-                </div>
-                <input type="submit"  name="Salvar" id="Salvar" value="Salvar" />
 
-                </div>
 
-            </form>
+                    <div class="enviar">
 
-        </div>
-        <!--container bg-->
+                        <input type="submit" name="Excluir" id="Excluir" value="Excluir">
+
+                        
+
+                        <input type="submit" name="Salvar" id="Salvar" value="Salvar" />
+
+                    </div>
+                    <div class="teste">
+                            <img src="imagens/icons8_ok_26px.png"></img>
+                        </div>
+                        <div class="limpa"></div>
+
+
+                </form>
+
+            </div>
+            <!--container bg-->
     </section>
     <!--cover-form-->
 
 
 
-     <script> var url="js/Altera_cliente.js";
-     $.getScript(url);
-     </script>
-     
- 
+    <script>
+        var url = "js/Altera_cliente.js";
+        $.getScript(url);
+    </script>
+
+
 </body>
 
 </html>
