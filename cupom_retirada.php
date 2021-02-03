fff91de733b2c9ca716ef373298927c9b97d4258
Gabriel-Miranda-98 <72518066+Gabriel-Miranda-98@users.noreply.github.com>
2021-02-03 19:45:43 -0300

    Cupom de retirada

---
+++ cupom_retirada.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="css/css_caixa.css">
+    <link rel="shortcut icon" type="image/x-icon" href="imagens/favicon.ico">
+
+    <title>Cupom Retirada</title>
+</head>
+
+<body>
+    <div class="center-90">
+        <section class="cover-form-90">
+            <div class="form-container-90">
+                <h1>CUPOM DE RETIRADA </h1>
+                <div class="form-wraper-90">
+                    <div class="col-90">
+                        <p id="empresa-90"></p>
+                        <p id="caixa-90"></p>
+                        <p id="valor"></p>
+                        <p id="Pessoa"></p>
+                        <p id="obs"></p>
+                        <p id="ass"></p>
+
+                    </div>
+                </div>
+                <div class="form-wraper-90">
+                    <div id="data_hora-90" class="data_hora-90">
+                        <p id="data_cupom"></p>
+                        <p id="hr_cupom"></p>
+
+                    </div>
+
+                </div>
+                <div class="form-wraper-90">
+                    <div class="col-90">
+                        <input type="button" id="btn" onclick="window.print()" value="Imprimir" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+</body>
+
+</html>
